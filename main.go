aa25b234ace50d53518009a623250424b9de9cef
nagri95 <nabil.grid.tet@gmail.com>
2019-10-21 15:25:33 +0200

    fourth commit

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handlerRequests() {
 	http.HandleFunc("/conservation/v1/country/", GBIF_APis.CountryHandler)
 	http.HandleFunc("/conservation/v1/species/", GBIF_APis.HandlerSpecies)
 	http.HandleFunc("/conservation/v1/diag/", GBIF_APis.HandlerDiag)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func main() {
@@ -20,8 +20,8 @@ func main() {
 	fmt.Println("heey")
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		log.Fatal("$PORT must be set")
 	}
 	handlerRequests()
-
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
